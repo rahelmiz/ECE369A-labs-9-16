c8390bdc83320ea9f060a871d871baa478b9137a
rahelmiz <rahlboo11@gmail.com>
2021-10-21 11:45:36 -0700

    Update Mask_Store.v

--- Mask_Store.v
+++ Mask_Store.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 10/19/2021 02:53:07 PM
+// Create Date: 10/19/2021 02:31:02 PM
 // Design Name: 
-// Module Name: Mask_Store
+// Module Name: MaskStore
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,7 +20,23 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Mask_Store(
-
-    );
+module MaskStore(MemAdr, MemData, RegData , Bytes2Store, Clk, out);
+    input [1:0] MemAdr;
+    input [31:0] MemData, RegData;
+    input Bytes2Store;
+    input Clk;
+    output reg out;
+    wire [31:0] Out_sh, Out_sh;
+    StoreByte sb(MemAdr, MemData, RegData, Clk, Out_sb);
+   
+    StoreHalf sh(MemAdr[1], MemData, RegData, Clk, Out_sh);
+    always @(*)  begin
+        case (Bytes2Store) 
+            2'd1:  out <= Out_sh;
+            2'd2: out <= Out_sb;
+            default: out <= MemData;
+        endcase
+    end
+    
 endmodule
+
